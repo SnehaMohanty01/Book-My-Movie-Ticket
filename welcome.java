c1f4d6d162ecc30366d699157dac9976bac062d9
Sneha Mohanty <snehamohanty1104@gmail.com>
2025-04-22 22:56:54 +0530

    welcome.java

---
+++ welcome.java
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Book My Ticket</title>
+    <link rel="stylesheet" href="welcome.css"> <!-- Link to external CSS for styling -->
+</head>
+<body>
+    <div class="welcome-container">
+        <header>
+            <h1>Welcome to Book My Ticket</h1>
+            <p>Your one-stop destination for movie tickets!</p>
+        </header>
+
+        <!-- Movie Image Gallery -->
+        <div class="movie-gallery">
+            <img src="images/venom.avif" alt="Venom: The Last Dance">
+            <img src="images/wild_robot.jpeg" alt="The Wild Robot">
+            <img src="images/smile2.jpg" alt="Smile 2">
+            <img src="images/alien.jpg" alt="Alien: Romulus">
+            <img src="images/nightmare.jpeg" alt="Tim Burton’s The Nightmare Before Christmas">
+            <img src="images/bhool.avif" alt="Bhool Bhulaiyaa 3">
+            <img src="images/singham3.avif" alt="Singham Again">
+            <img src="images/do.jpeg" alt="Do Patti">
+        </div>
+
+        <!-- Book My Ticket Button -->
+        <div class="action-button">
+            <a href="index.html" class="book-ticket-button">Book My Ticket</a>
+        </div>
+    </div>
+</body>
+</html>
